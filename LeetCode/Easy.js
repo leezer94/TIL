8798fc491d32690d4053f3440fcc0b9535d23976
leezer94 <2kunhee94@gmail.com>
2021-12-02 03:15:50 +0900

    leetCode removeDuplicates

--- LeetCode/Easy.js
+++ LeetCode/Easy.js
@@ -116,5 +116,26 @@
   };
 
   let s = '[{()}]';
-  console.log(isValid(s));
+  // console.log(isValid(s));
+}
+{
+  const removeDuplicates = function (nums) {
+    let uniqueCursor = 1;
+
+    // Loop through the array of number
+    for (let i = 1; i < nums.length; i++) {
+      // Find unique values
+      if (nums[i] !== nums[i - 1]) {
+        // Move unique values to follow the last unique value found
+        nums[uniqueCursor] = nums[i];
+
+        // Move the unique value cursor onward
+        uniqueCursor++;
+      }
+    }
+    return uniqueCursor;
+  };
+
+  let nums = [1, 1, 2];
+  console.log(removeDuplicates(nums));
 }
