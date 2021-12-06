a005a733459a79e0dfa744432fd441d71a09fc0e
leezer94 <2kunhee94@gmail.com>
2021-12-07 03:19:26 +0900

    leetCode Plus One

--- LeetCode/Easy.js
+++ LeetCode/Easy.js
@@ -187,5 +187,23 @@
   };
 
   let s = '   fly me   to   the moon  ';
-  console.log(lengthOfLastWord(s));
+  // console.log(lengthOfLastWord(s));
+}
+{
+  const plusOne = function (digits) {
+    for (let i = digits.length - 1; i >= 0; i--) {
+      if (digits[i] < 9) {
+        digits[i] = digits[i] + 1;
+        return digits;
+      } else {
+        digits[i] = 0;
+      }
+    }
+    //  [9,9] => [1,0,0]
+    digits.unshift(1);
+    return digits;
+  };
+
+  let digits = [1, 2, 9];
+  // console.log(plusOne(digits));
 }
