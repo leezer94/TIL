1358e91df5b8a6ca8e418eee9d276e2cff92f514
leezer94 <2kunhee94@gmail.com>
2021-12-24 03:17:41 +0900

    Udemy JavaScript BootCamp

---
+++ Udemy/JS-Bootcamp/README.md
@@ -0,0 +1,226 @@
+# Udemy JavaScript bootcamp
+
+`== Double Equals`
+
+- Checks for equality of value, but not equality of type
+- It coerces(converts) both values to the smae type and then compares them
+- This can lead to some unexpected result
+
+`=== Triple Equals`
+
+- Checks for equality fo value and type
+
+`!== None Equality`
+
+- Strictly not Equals
+
+`Truthy and Falsy Values`
+
+- All values have an inherent truthy or falsy boolean value
+
+`Falsy Values`
+
+- false
+- 0
+- '' (Empty String)
+- null
+- undefined
+- NaN
+
+`Truthy Values`
+
+- Everything else is truthy
+
+`Operator Precednece` <br>
+link : https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Operator_Precedence
+
+- NOT(!) has higher precedence than && and || && has higher precedence than ||
+- we can alter this using ()
+
+`Switch Statement`
+
+- looking for the first match and then as soon as it finds it, everything else runs
+
+`Ternary Operator`
+
+- condition ? ecpIfTure : expIfFalse
+- `If(?)` condition is Ture this code runs `otherwise(:)` that code runs
+- only for simple code
+
+`Arrays`
+
+Array Method : https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array
+
+- Arrays are indexed
+- Arrays are mutable
+- Arrays are special object where the keys are an ordered set of indices numbers starting at zero
+- array.push() method actually mutate original value
+- array.unshift() method can add multiple values to the front of arrays
+- array.concat() methid can pass in multiple values
+
+`Referecne Type`
+
+Value Type Variable
+
+- instead of storing all of those values in the variable itself, what JavaScript does instead is it stores reference
+
+`Primitive Type`
+
+- Primitive Type are stored as the actual value in a variable arrays are different
+
+`const And Arrays`
+
+- Value can change as long as the reference ramians the same
+- because Arrays are `reference type` `const` is use most of the time when we work with Arrays
+
+`Object`
+
+- Objects are collections of properties.
+- Properties are a Key - Value pair
+- Rather than accessing data using an index, we use custom keys.
+
+                  KEY   :   VALUE
+               Username : 'crazyCatLady'
+               upvotes  : 7
+                  text  : 'great post!'
+
+- All keys are converted to strings
+- Sometimes we have property names that may not be vaild JavaScript identifiers then we use Array Brackets `['example']`;
+
+          `const palette = {
+            red: '#eb4d4b',
+            yellow: '#f9ca24',
+            blue: '#30336b',
+            };
+
+            let mysteryColor = 'yellow';
+
+            console.log(palette[mysteryColor]);`
+
+- This looks for the value of mysteryColor and then plugs into palette
+- Try to use `Dot Notation` whenever possible but for the `dynamic value` we need to use array bracket
+
+`Object and Reference Type`
+
+- Same as Array better call it with const and we can change what's inside of Object but we don't change that Object
+
+`Array / Obejct Equality`
+
+- Eventhough they contains exact same values and the exact same order, but they are completly different in JavaScript
+
+            let array1 = [1,2,3];
+            let array2 = [1,2,3];
+            array1 === array2 => false
+
+ex)
+array1 => 12342343
+array2 => 35234324
+
+- when we have Variable the value of variable dosent store the Array it stores the reference
+- Because Array can be huge so JavaScript dosent store Array itself
+- All that ('===','==') Operators will do is check for quality fo REFERENCE not QUALITY
+
+`Loops`
+
+- Loop allow us to repeat code
+  - Print 'hello' 10 times
+  - Sum all numbers in array
+- There are multiple types
+
+  - for loop
+  - while loop
+  - for ... of loop
+  - for ... in loop
+
+`For loop`
+
+         for(
+            [initialExpression];
+            [condition];
+            [incrementExpression];
+         ){
+
+         }
+
+`Nested for loop`
+
+- Every single time this outer loops runs just a single iteration, the inner loop completes if full cycle
+
+            for (let i = 0; i <= 10; i++) {
+               console.log(`outer loops : ${i}`);
+               for (let j = 10; j >= 0; j -= 2) {
+                console.log(`   inner Loops : ${j}`);
+               }
+            }
+
+`Break`
+
+- We can use instead of loops to break out of that loop
+- whenever JavaScript encounters break, that loop that its enclosed in is done.
+- it is not common in for loop but it is common in while loop
+
+`for ... of with Object`
+
+- there are no indecies in the Object
+- which is Object is not iterable
+- by using Object.keys / Object.values we can get iterated Keys / Values
+
+`for ... in`
+
+      - difference between for...in and for...of - for...of will iterate over the actual values in an array or an iterable or string
+      - for ...in will loop over the keys, the properties in an Object
+
+`Funtions ( reusable procedures )`
+
+- Functions allow us to write resusable, modular code
+- We define a 'chunk\ of code that we can then execute at a later point
+- We use them ALL THE Time
+
+`SCOPE`
+
+- Variable 'visibility'
+
+  - The location where a variable is defined dictates where we have access to that variable
+
+1. Block Scope
+   ]
+
+   - There is no Block Scope for `Var`
+
+2. Lexical Scope
+
+   - Nested functions are lexically bound, they are bound to the scope of their parent or of their outer functions
+   - One way relation ship `inside -> outside`
+
+3. Funtion expressions
+
+   - another way to express functions
+     ex ) Arrow Function, ...
+   - In JavaScript functions are Object so we can put them in the Variable, we can stroe 10 of thme in array
+
+4. Higer order Funtions
+
+   - Functions that operate on/with other functions
+   - Accept other functions as arguments
+   - return a function
+   - We can put functions in the array and loop over it that we don't have to hard code it
+   - We can call them dynamically
+
+5. Callback functions
+
+   - Anytime we pass a function to another function and it's executed in that function we would call it Callback function
+
+`Array Callback Methods`
+
+Map()
+
+- Creates a new `Array` with the results fo calling a callback on every element in the array
+- The original Array is immutable
+
+find()
+
+- Returns the value of the first element in the array that satisfies the proveided testing function.
+
+`Implicit return`
+
+- Adding an implicit return to an arrow function means that you don't have to write the return keyword itself
---
+++ Udemy/JS-Bootcamp/index.js
@@ -0,0 +1,787 @@
+{
+  // Nesting Conditionals
+
+  let password = '';
+
+  if (password.length >= 6) {
+    if (password.indexOf(' ') === -1) {
+      console.log('Valid Password');
+    } else {
+      console.log('Password is long enough, but cannnot contain spaces');
+    }
+  } else {
+    console.log('Password must be longer');
+  }
+
+  // Truthy and Falsy values
+
+  let loggedInUser = null;
+
+  if (loggedInUser) {
+    console.log('YOU ARE LOGGED IN');
+  } else {
+    console.log('PLEASE LOG IN !');
+  }
+}
+{
+  // Switch Statement
+
+  let day = 3;
+
+  if (day === 1) {
+    console.log('Monday');
+  } else if (day === 2) {
+    console.log('Tuesday');
+  } else if (day === 3) {
+    console.log('Wednesday');
+  } else if (day === 4) {
+    console.log('Thursday');
+  } else if (day === 5) {
+    console.log('Friday');
+  } else if (day === 6) {
+    console.log('Saturday');
+  } else if (day === 7) {
+    console.log('Sunday');
+  } else {
+    console.log(`Invalid Day`);
+  }
+
+  switch (day) {
+    case 1:
+      console.log('Monday');
+      break;
+    case 2:
+      console.log('Tuesday');
+      break;
+    case 3:
+      console.log('Wednesday');
+      break;
+    case 4:
+      console.log('Thursday');
+      break;
+    case 5:
+      console.log('Friday');
+      break;
+    case 6:
+      console.log('Saturday');
+      break;
+    case 7:
+      console.log('Sudday');
+    default:
+      console.log('Invalid day');
+  }
+
+  let emoji = 'sad face';
+  switch (emoji) {
+    case 'sad face':
+    case 'happy face':
+      console.log('yellow');
+      break;
+
+    case 'lips':
+      console.log('purple');
+      break;
+    case 'aubergine':
+      console.log('green');
+      break;
+  }
+}
+{
+  // Tenary Operator
+  let num = 7;
+
+  if (num === 7) {
+    console.log(`lucky`);
+  } else {
+    console.log('bad');
+  }
+
+  num === 7 ? console.log('lucky') : console.log('bad');
+
+  let status = 'online';
+
+  let color = status === 'offline' ? 'red' : 'green';
+
+  // console.log(color);
+}
+{
+  // Arrays and Objects
+
+  // Arrays
+  let shoppingList = ['cereal', 'milk', 'ice'];
+
+  let lotto = [45, 12, 23, 45, 34];
+
+  let myCollection = [12, 'dog', true, null, NaN];
+
+  let colors = ['red', 'blue', 'green', 'violet', 'indigo'];
+
+  colors[3] = 'milk';
+
+  console.log(colors); // output = ['red', 'blue', 'green', 'milk', 'indigo'];
+
+  // add to the end of array
+
+  colors[colors.length] = 'tata'; // same as array.push() method
+
+  console.log(colors); // output = ['red', 'blue', 'green', 'milk', 'indigo', 'tata']
+
+  //concat
+
+  let fruits = ['apple', 'banana'];
+  let veggies = ['asparagus', 'brussel sprouts'];
+
+  console.log(fruits.concat(veggies));
+
+  // includes returns boolean
+
+  let ingredients = ['water', 'corn starch', 'flour', 'brown sugar', 'shrimp'];
+
+  if (ingredients.includes('flour') === true) {
+    console.log(' I AM GLUTEN FREE I CANNOT EAT THAT');
+  }
+
+  // replace the value using splice
+
+  let animals = ['shark', 'dog', 'monkey', 'hippo', 'whale', 'bear', 'lizard'];
+
+  console.log(animals.splice(0, 2, 'shark!', 'OCTOPUS'));
+
+  console.log(animals);
+
+  // Sort
+  let poeple = ['keonhee', 'ronja', 'mike', 'michale', 'lamsey'];
+
+  let nums = [34, 10, 10000, 67, 99];
+
+  // Const and Arrays
+
+  const myEggs = ['brown', 'brown'];
+
+  myEggs.push('purple');
+
+  myEggs[0] = 'green';
+
+  //   myEggs = ['green', 'purple'];  // this cannot muate the array
+
+  // Nested Arrays  ( multi dimentional Array)
+
+  const board = [
+    ['O', null, 'X'],
+    [null, 'X', 'O'],
+    ['X', 'O', null],
+  ];
+  console.clear();
+}
+{
+  // Objects
+
+  const fitBitData = {
+    totalSteps: 308727,
+    totalMiles: 211.7,
+    avgCalorieBurn: 5755,
+    woroutsThisWeek: '5 of 7',
+    avgGoodSleep: '2:13',
+    45: 'forty five',
+  };
+
+  // accessing to object
+  console.log(fitBitData.totalSteps);
+  console.log(fitBitData.avgGoodSleep);
+  console.log(fitBitData['45']);
+  console.log(fitBitData[45]);
+
+  // array brackets
+
+  const numbers = {
+    100: 'one hundred',
+    16: 'sixteen',
+    '76 trombones': 'great song!',
+  };
+
+  console.log(numbers[100]);
+  console.log(numbers['100']);
+  console.log(numbers['76 trombones']);
+
+  // this looks for the value of 'mysteryColor' and plugs into palette
+  const palette = {
+    red: '#eb4d4b',
+    yellow: '#f9ca24',
+    blue: '#30336b',
+  };
+
+  let mysteryColor = 'yellow';
+
+  console.log(palette[mysteryColor]);
+
+  //Updating , Adding properites
+
+  const userReviews = {};
+
+  userReviews.queenBee49 = 4.5;
+  userReviews['mrSmith78'] = 3.5;
+  userReviews.keonhee = 28;
+
+  userReviews.queenBee49 += 5.5;
+  userReviews['mrSmith78']++;
+
+  console.log(userReviews);
+
+  // Arrays + Object
+
+  const student = {
+    firstName: 'keonhee',
+    lastName: 'Lee',
+    strengths: ['Music', 'Art'],
+    exams: {
+      midterm: 92,
+      final: 88,
+    },
+  };
+
+  const exams = student['exams'];
+  const average = (exams.midterm + exams.final) / 2;
+  console.log(average);
+
+  const shoppingCart = [
+    {
+      product: 'Jenga Classic',
+      price: 6.88,
+      quantity: 1,
+    },
+    {
+      product: 'Echo Dot',
+      price: 29.99,
+      quantity: 3,
+    },
+    {
+      product: 'Jenga Classic',
+      price: 39.99,
+      quantity: 2,
+    },
+  ];
+
+  for (const items of shoppingCart) {
+    // console.log(items);
+  }
+
+  const game = {
+    player1: {
+      username: 'Blue',
+      playingAs: 'X',
+    },
+    player2: {
+      username: 'Muffins',
+      playingAs: 'X',
+    },
+    board: [
+      ['O', null, 'X'],
+      ['X', 'X', 'O'],
+      ['X', 'O', null],
+    ],
+  };
+}
+
+{
+  // Arrays and Object Equality
+
+  let nums = [1, 2, 3];
+  let mystery = [1, 2, 3];
+
+  console.log(nums === mystery); // output false
+
+  let moreNums = nums;
+
+  console.log(nums === moreNums); // true
+
+  const user = {
+    username: 'CherryGarcia8',
+    email: 'fdskjf@gamil.com',
+    notifications: [],
+  };
+
+  if (!user.notifications) {
+    console.log('No New Notifications');
+  } // it works
+
+  if (!user.notifications === []) {
+    console.log('Yes New Notifications');
+  } // dosent work
+  console.clear();
+}
+{
+  // Loops ( repeating Code )
+  for (let num = 1; num <= 20; num++) {
+    // console.log(`${num} x ${num} : ${num * num}`);
+  }
+
+  // loops and Arrays
+
+  const examScores = [98, 77, 84, 91, 57, 66];
+
+  for (let i = 0; i < examScores.length; i++) {
+    // console.log(examScores[i]);
+  }
+
+  const myStudents = [
+    {
+      firstName: 'keonhee',
+      grade: 85,
+    },
+    {
+      firstName: 'ronhja',
+      grade: 25,
+    },
+    {
+      firstName: 'mika',
+      grade: 93,
+    },
+    {
+      firstName: 'dels',
+      grade: 23,
+    },
+  ];
+
+  let total = 0;
+
+  for (const student of myStudents) {
+    total += student.grade;
+  }
+  // console.log(total / myStudents.length);
+
+  // Nested for loops
+
+  for (let i = 0; i <= 10; i++) {
+    // console.log(`outer loops : ${i}`);
+    for (let j = 10; j >= 0; j -= 2) {
+      // console.log(`   inner Loops : ${j}`);
+    }
+  }
+
+  const gameBoard = [
+    [4, 32, 8, 4],
+    [64, 8, 32, 2],
+    [8, 32, 16, 4],
+    [2, 8, 4, 2],
+  ];
+
+  let totalScore = 0;
+  for (let i = 0; i < gameBoard.length; i++) {
+    let row = gameBoard[i];
+    for (let j = 0; j < row.length; j++) {
+      totalScore += row[j];
+    }
+  }
+
+  // console.log(totalScore);
+
+  // while loop
+
+  const target = Math.floor(Math.random() * 10);
+
+  let guess = Math.floor(Math.random() * 10);
+
+  let count = 0;
+
+  // while (guess !== target) {
+  //   // console.log(`Target : ${target}, Guess : ${guess}`);
+
+  //   guess = Math.floor(Math.random() * 10);
+  //   count++;
+  // }
+
+  // Break;
+
+  for (let i = 0; i < 10; i++) {
+    // console.log(i);
+
+    if (i > 5) {
+      continue;
+    }
+  }
+
+  while (true) {
+    if (target === guess) break;
+    // console.log(`Target : ${target}, Guess : ${guess}`);
+    guess = Math.floor(Math.random() * 10);
+    count++;
+  }
+  // console.log(`Target : ${target}, Guess : ${guess}`);
+  // console.log(`you win in ${count + 1} try`);
+
+  // for ... of
+
+  const magicSquare = [
+    [2, 7, 6],
+    [9, 5, 1],
+    [4, 3, 8],
+  ];
+
+  for (const row of magicSquare) {
+    let sum = 0;
+    for (const num of row) {
+      sum += num;
+    }
+    console.log(`${row} summed to ${sum}`);
+  }
+
+  // for ... of with Object
+
+  const movieReviews = {
+    Arrival: 9.5,
+    Alien: 100,
+    Amile: 33,
+    ajejejdm: 333,
+    Bekdise: 33,
+    Ckdsej: 122,
+  };
+
+  let movieArr = Object.keys(movieReviews);
+  let movieRates = Object.values(movieReviews);
+
+  for (const movie of movieArr) {
+    // console.log(movie);
+  }
+
+  for (const rates of movieRates) {
+    total += rates;
+  }
+
+  // console.log(total / movieRates.length);
+
+  // for... in
+
+  const jeopardyWinnings = {
+    regularPlay: 2123123,
+    watsonChallenges: 21323,
+    tournermentOfChapions: 2123,
+  };
+
+  for (const prorperty in jeopardyWinnings) {
+    console.log(prorperty); //output : keys
+    console.log(jeopardyWinnings[prorperty]); // values
+  }
+
+  for (const k in [88, 99, 77, 66]) {
+    console.log(k); // output : index
+  }
+  console.clear();
+}
+{
+  // Functions
+
+  function rollDie() {
+    let roll = Math.floor(Math.random() * 6) + 1;
+    console.log(roll);
+  }
+
+  function throwDie() {
+    for (let i = 0; i < 5; i++) {
+      rollDie();
+    }
+  }
+
+  let name = 'keonhee';
+
+  function greet(name) {
+    console.log(`Hi! ${name}`);
+  }
+
+  function isValidPassword(password, username) {
+    let isValid = false;
+    password = password.toString();
+
+    if (password.length < 8) {
+      isValid;
+    } else if (password.includes(' ')) {
+      isValid;
+    } else if (password.includes(username)) {
+      isValid;
+    } else {
+      isValid = true;
+    }
+
+    return isValid;
+  }
+
+  // console.log(isValidPassword('dogLuvr123!', 'dogLuvr'));
+
+  function getAvg(arr) {
+    let total = arr.reduce((a, b) => a + b);
+
+    return Math.floor(total / arr.length);
+  }
+
+  // console.log(getAvg([100, 200, 300, 100, 200]));
+
+  function getCard() {
+    const values = [2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'K', 'Q', 'A'];
+    const suits = ['spades', 'hearts', 'diamonds'];
+    let value = Math.floor(Math.random(values) * values.length);
+    let suit = Math.floor(Math.random(suits) * suits.length);
+
+    return { value: `${values[value]}`, suit: `${suits[suit]}` };
+  }
+
+  console.log(getCard());
+  console.clear();
+}
+{
+  // Advanced Look at functions
+
+  // Block Scope
+  var animal = 'arrow';
+  if (true) {
+    // console.log(animal);
+  }
+
+  // console.log(animal);
+
+  // Lexical Scope
+
+  function outer() {
+    let movie = 'Amadeus';
+
+    function inner() {
+      console.log(movie.toUpperCase());
+    }
+
+    inner();
+  }
+
+  // console.log(outer());
+
+  // Funtion expressions
+
+  function add1(x, y) {
+    return x + y;
+  }
+
+  const add2 = function (x, y) {
+    return x + y;
+  };
+
+  const add3 = (x, y) => {
+    return x + y;
+  };
+
+  const product = function sum(x, y) {
+    return x + y;
+  };
+
+  // Higer order Functions
+  function add5(x, y) {
+    return x + y;
+  }
+  const substract = function (x, y) {
+    return x - y;
+  };
+  const multiply = function (x, y) {
+    return x * y;
+  };
+  const divide = function (x, y) {
+    return x / y;
+  };
+
+  const operations = [add5, substract, multiply, divide];
+
+  for (const func of operations) {
+    let result = func(100, 3);
+
+    // console.log(result);
+  }
+
+  const thing = {
+    doSomthing: multiply,
+  };
+
+  // console.log(thing.doSomthing(50, 2));
+
+  // Functions as arguments
+  function repeatNTimes(action, number) {
+    for (let i = 0; i < number; i++) {
+      action();
+    }
+  }
+
+  function pickOne(func1, func2) {
+    let randomNumber = Math.random();
+    console.log(randomNumber);
+    if (randomNumber < 0.5) {
+      func1();
+    } else func2();
+  }
+
+  function hate() {
+    console.log('i hate this shit');
+  }
+
+  function love() {
+    console.log('i love this shit');
+  }
+
+  // pickOne(hate, love);
+
+  // Funtions as return value
+
+  // const triple = multiplyBy(3);
+  // triple(5); // 15
+
+  // const double = multiplyBy(2);
+  // double(8); // 16
+
+  function multiplyBy(num) {
+    return function (x) {
+      return x * num;
+    };
+  }
+
+  const triple = multiplyBy(3);
+  const double = multiplyBy(2);
+  const halve = multiplyBy(0.5);
+
+  function makeBetweenFunc(from, to) {
+    return function (number) {
+      return number >= from && number <= to;
+    };
+  }
+
+  const isChild = makeBetweenFunc(0, 18);
+  const isNiceWeather = makeBetweenFunc(15, 28);
+
+  // console.log(isChild(1));
+
+  // Callbacks
+
+  const stop = () => {
+    alert('go away');
+  };
+
+  // setTimeout(stop, 5000);
+
+  // using annonymous function
+
+  // setTimeout(() => {
+  //   alert('who are you');
+  // }, 5000);
+
+  const eventbutton = document.querySelector('#event-button');
+
+  eventbutton.addEventListener('click', () => {
+    setTimeout(() => {
+      alert('dont click me');
+    }, 500);
+  });
+}
+{
+  // Array Callback Methods
+
+  // forEach
+  const numbers = [20, 21, 22, 23, 24, 25, 26, 27, 28];
+
+  numbers.forEach((num, i) => {
+    // console.log(`numnber : ${num}`, `index : ${i}`);
+  });
+
+  //  Map
+
+  const words = ['asap', 'byob', 'rsvp', 'diy'];
+
+  const doubles = numbers.map((num) => {
+    return num * 2;
+  });
+
+  const whichNum = numbers.map((num) => {
+    if (num % 2 === 0) return 'even';
+
+    return 'odd';
+  });
+
+  const abbreviation = words.map((word) => {
+    return word.toUpperCase().split('').join('.');
+  });
+
+  // console.log(abbreviation);
+
+  // Implicit return
+
+  const square = (n) => n * n;
+
+  // find();
+
+  let movies = ['Spider Man', 'About Time', 'Mr. Deeds', 'Fantastic Four'];
+
+  const movie = movies.find((movie) => {
+    return movie.includes('S');
+  });
+
+  // filter();
+
+  const nums = [11, 22, 33, 44, 55, 66, 77, 88, 99, 100];
+
+  const oddNum = nums.filter((num) => num % 2 !== 0);
+
+  const books = [
+    {
+      title: 'Good Omens',
+      authors: ['Terry Pratchett', 'Neil Gaiman'],
+      rating: 4.25,
+      genres: ['fiction', 'fantasy'],
+    },
+    {
+      title: 'Changing My Mind',
+      authors: ['Zadie Smith'],
+      rating: 3.83,
+      genres: ['nonfiction', 'essays'],
+    },
+    {
+      title: 'Bone: The Complete Edition',
+      authors: ['Jeff Smith'],
+      rating: 4.42,
+      genres: ['fiction', 'graphic novel', 'fantasy'],
+    },
+    {
+      title: 'American Gods',
+      authors: ['Neil Gaiman'],
+      rating: 4.11,
+      genres: ['fiction', 'fantasy'],
+    },
+    {
+      title: 'A Gentleman in Moscow',
+      authors: ['Amor Towles'],
+      rating: 4.36,
+      genres: ['fiction', 'historical fiction'],
+    },
+    {
+      title: 'The Name of the Wind',
+      authors: ['Patrick Rothfuss'],
+      rating: 4.54,
+      genres: ['fiction', 'fantasy'],
+    },
+    {
+      title: 'The Overstory',
+      authors: ['Richard Powers'],
+      rating: 4.19,
+      genres: ['fiction', 'short stories'],
+    },
+    {
+      title: 'The Way of Kings',
+      authors: ['Brandon Sanderson'],
+      rating: 4.65,
+      genres: ['fantasy', 'epic'],
+    },
+    {
+      title: 'Lord of the flies',
+      authors: ['William Golding'],
+      rating: 3.67,
+      genres: ['fiction'],
+    },
+  ];
+
+  const genre = books.filter((book) => book.genres.includes('fiction'));
+  const shortForm = books.filter((book) =>
+    book.genres.includes('short stories')
+  );
+
+  console.log(shortForm);
+}
