b20a01e1f462bf3f923267b78b3f797ff21d4dff
leezer94 <2kunhee94@gmail.com>
2021-12-31 03:39:45 +0900

    Udemy JS BootCamp

--- Udemy/JS-Bootcamp/index2.js
+++ Udemy/JS-Bootcamp/index2.js
@@ -29,4 +29,61 @@ for (const color of colors) {
   box.addEventListener('click', changeColor);
 }
 
-document.body.addEventListener('click', function () {});
+const input = document.querySelector('#username');
+const items = document.querySelector('#items');
+
+input.addEventListener('keypress', function (e) {
+  if (e.key === 'Enter') {
+    if (!this.value) return;
+
+    const newItemValue = this.value;
+
+    const newItem = document.createElement('li');
+    newItem.innerText = newItemValue;
+
+    items.appendChild(newItem);
+
+    this.value = '';
+  }
+});
+
+// form
+
+const form = document.querySelector('#signup-form');
+const creditCardInput = document.querySelector('#cc');
+const termsCheckbox = document.querySelector('#terms');
+const veggieSelect = document.querySelector('#veggie');
+
+// form.addEventListener('submit', function (e) {
+//   e.preventDefault();
+
+//   console.log(`cc ${creditCardInput.value}`);
+//   console.log(`terms ${termsCheckbox.checked}`);
+//   console.log(`veggieSelect ${veggieSelect.value}`);
+// });
+
+const formData = {};
+
+for (let input of [creditCardInput, termsCheckbox, veggieSelect]) {
+  input.addEventListener('change', ({ target }) => {
+    const { name, type, value, checked } = target;
+    formData[name] = type === 'checkbox' ? checked : value;
+    console.log(formData);
+  });
+}
+// creditCardInput.addEventListener('input', (e) => {
+//   console.log(`CC Changed ${e}`);
+
+//   formData['CC'] = e.target.value;
+// });
+
+// veggieSelect.addEventListener('input', (e) => {
+//   console.log('Veggie', e);
+
+//   formData['veggie'] = e.target.value;
+// });
+
+// termsCheckbox.addEventListener('input', (e) => {
+//   console.log('checkbox', e);
+//   formData['agreeToTerms'] = e.target.checked;
+// });
