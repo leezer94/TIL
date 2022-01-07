5685ef642111331855dc2332f57586c54be18d73
leezer94 <2kunhee94@gmail.com>
2022-01-07 19:05:11 +0900

    feat: change the value of walls depends on directions depending on user directions change the value of walls true which is visited

--- Udemy/JS-Bootcamp/Project 3/app.js
+++ Udemy/JS-Bootcamp/Project 3/app.js
@@ -5,7 +5,7 @@ const { Engine, Render, Runner, World, Bodies } = Matter;
 // Constant
 const width = 500;
 const height = 500;
-const cells = 5;
+const cells = 3;
 
 const engine = Engine.create();
 const { world } = engine;
@@ -35,6 +35,22 @@ World.add(world, walls);
 
 // Maze generation
 
+const shuffle = (arr) => {
+  let counter = arr.length;
+
+  while (counter > 0) {
+    const index = Math.floor(Math.random() * counter);
+
+    counter--;
+    // randomly shuffling array's index
+    const temp = arr[counter];
+    arr[counter] = arr[index];
+    arr[index] = temp;
+  }
+
+  return arr;
+};
+
 // creates an empty array has 3 possible places in it
 
 // Abstract Maze demensions
@@ -51,6 +67,58 @@ const horizontals = Array(cells - 1)
   .fill(null)
   .map(() => Array(cells).fill('false'));
 
-console.log(grid);
+// Starting Point
+const startRow = Math.floor(Math.random() * cells);
+const startColumn = Math.floor(Math.random() * cells);
+
+const stepThoroughCell = (row, column) => {
+  // If i have visited the cell a [row,column], then return
+  if (grid[row][column] === true) return;
+  // Mark this cell as visited (true)
+  grid[row][column] = true;
+  // Assemble randomly listed order neighbors
+  const neighbors = shuffle([
+    [row - 1, column, 'up'],
+    [row, column + 1, 'right'],
+    [row + 1, column, 'down'],
+    [row, column - 1, 'left'],
+  ]);
+
+  // forEach neighbor
+
+  for (const neighbor of neighbors) {
+    const [nextRow, nextColumn, direction] = neighbor;
+
+    // See if that neigbor is out of bounce
+    if (
+      nextRow < 0 ||
+      nextRow >= cells ||
+      nextColumn < 0 ||
+      nextColumn >= cells
+    ) {
+      continue;
+    }
+    // If we have visited that neighbor, continue to next neighbor
+    if (grid[nextRow][nextColumn] === true) {
+      continue;
+    }
+    // Remove a wall from either horizontals, or verticals
+    console.log(direction);
+    if (direction === 'left') {
+      verticals[row][column - 1] = true;
+    } else if (direction === 'right') {
+      verticals[row][column] = true;
+    } else if (direction === 'up') {
+      horizontals[row - 1][column] = true;
+    } else if (direction === 'down') {
+      horizontals[row][column] = true;
+    }
+  }
+
+  // Visit that next cell
+};
+
+stepThoroughCell(startRow, startColumn);
+
 console.log(verticals);
 console.log(horizontals);
