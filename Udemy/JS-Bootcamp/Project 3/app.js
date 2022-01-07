a88b3def426a20ab6d4a35390b337a0b7a46cc11
leezer94 <2kunhee94@gmail.com>
2022-01-07 15:05:13 +0900

    feat: add shapes and features rectangle and circle shapes are added and those are moveable by click and dragging them shapes are generated randomly by the width and height of canvas

--- Udemy/JS-Bootcamp/Project 3/app.js
+++ Udemy/JS-Bootcamp/Project 3/app.js
@@ -1,6 +1,11 @@
-const { Engine, Render, Runner, World, Bodies } = Matter;
+const { Engine, Render, Runner, World, Bodies, MouseConstraint, Mouse } =
+  Matter;
 
 // BoilerPlate
+
+// Constant
+const width = 800;
+const height = 600;
 
 const engine = Engine.create();
 const { world } = engine;
@@ -8,13 +13,22 @@ const render = Render.create({
   element: document.body,
   engine: engine,
   options: {
-    width: 800,
-    height: 600,
+    wireframes: false,
+    width,
+    height,
   },
 });
 
 Render.run(render);
 Runner.run(Runner.create(), engine);
+
+// Click and dragging
+World.add(
+  world,
+  MouseConstraint.create(engine, {
+    mouse: Mouse.create(render.canvas),
+  })
+);
 
 // Walls
 
@@ -26,3 +40,27 @@ const walls = [
 ];
 
 World.add(world, walls);
+
+// Random Shapes
+
+for (let i = 0; i < 50; i++) {
+  if (Math.random() > 0.5) {
+    // Create Rectangles
+    World.add(
+      world,
+      Bodies.rectangle(Math.random() * width, Math.random() * height, 50, 50, {
+        isStatic: false,
+      })
+    );
+  } else {
+    // Create Cricles
+    World.add(
+      world,
+      Bodies.circle(Math.random() * width, Math.random() * height, 30, {
+        render: {
+          fillStyle: '',
+        },
+      })
+    );
+  }
+}
