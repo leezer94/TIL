4e55c1259be785af3345f70cfa62abbcaa2fcd1d
leezer94 <2kunhee94@gmail.com>
2022-01-11 12:23:05 +0900

    feat: add email form

--- Udemy/JS-Bootcamp/project6/app.js
+++ Udemy/JS-Bootcamp/project6/app.js
@@ -1,1 +1,19 @@
-s
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send(`
+  <div>
+      <form action="">
+        <input placeholder="email" />
+        <input placeholder="password" />
+        <input placeholder="password confirmation" />
+        <button>Sign up</button>
+      </form>
+    </div>
+  `);
+});
+
+app.listen(3000, () => {
+  console.log('i am working now ');
+});
