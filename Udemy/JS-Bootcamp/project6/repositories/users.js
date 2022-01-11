8efaae30b79584ba737ed6fa1fc025024a66284c
leezer94 <2kunhee94@gmail.com>
2022-01-11 15:31:16 +0900

    feat: add function that generates userId randomly by using crypto.randomBytes

--- Udemy/JS-Bootcamp/project6/repositories/users.js
+++ Udemy/JS-Bootcamp/project6/repositories/users.js
@@ -1,4 +1,5 @@
 const fs = require('fs');
+const crypto = require('crypto');
 
 class UsersRepository {
   constructor(filename) {
@@ -23,11 +24,37 @@ class UsersRepository {
       })
     );
   }
+
+  async create(attrs) {
+    attrs.id = this.randomId();
+    // {email : 'ddd@fasf.com' , password : 'password'}
+
+    const records = await this.getAll();
+    records.push(attrs);
+
+    await this.writeAll(records);
+  }
+
+  async writeAll(records) {
+    // write the updated 'records' array back to this.filename
+
+    await fs.promises.writeFile(
+      this.filename,
+      // custom format value of null
+      // 2 : level of indentation
+      JSON.stringify(records, null, 2)
+    );
+  }
+
+  randomId() {
+    return crypto.randomBytes(4).toString('hex');
+  }
 }
 
 const test = async () => {
   const repo = new UsersRepository('users.json');
 
+  await repo.create({ email: '2kunhee94@gmail.com', password: 'password' });
   const users = await repo.getAll();
 
   console.log(users);
