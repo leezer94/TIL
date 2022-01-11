8f340302e2b61b8d7c6d6dd98fb692c9d38245e7
leezer94 <2kunhee94@gmail.com>
2022-01-11 13:06:50 +0900

    refactor: simplize code by using library by using body-parse library simplize hard-coded parsebody function and automatically body-parse

--- Udemy/JS-Bootcamp/project6/app.js
+++ Udemy/JS-Bootcamp/project6/app.js
@@ -1,5 +1,10 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+
 const app = express();
+
+// Automatically body-parse by using Library
+app.use(bodyParser.urlencoded({ extneded: true }));
 
 app.get('/', (req, res) => {
   res.send(`
@@ -14,28 +19,8 @@ app.get('/', (req, res) => {
   `);
 });
 
-const bodyParser = (req, res, next) => {
-  if (req.method === 'POST') {
-    // simillar to addEventListener
-
-    req.on('data', (data) => {
-      const parsed = data.toString('utf8').split('&');
-      const formData = {};
-      for (const pair of parsed) {
-        const [key, value] = pair.split('=');
-        formData[key] = value;
-      }
-
-      req.body = formData;
-      next();
-    });
-  } else {
-    next();
-  }
-};
-
 // this tells router to watch for an incomming request with a path f forward / and a method of post
-app.post('/', bodyParser, (req, res) => {
+app.post('/', (req, res) => {
   console.log(req.body);
   // get acces to eamil.password, confirmation
   res.send('Account created!!');
