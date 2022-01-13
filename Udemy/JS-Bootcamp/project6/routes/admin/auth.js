43e2803c7f4ede11b85a36dcc81735d570f8c8d6
leezer94 <2kunhee94@gmail.com>
2022-01-13 12:49:22 +0900

    add password validator

--- Udemy/JS-Bootcamp/project6/routes/admin/auth.js
+++ Udemy/JS-Bootcamp/project6/routes/admin/auth.js
@@ -8,6 +8,8 @@ const {
   requireEmail,
   requirePassword,
   requirePasswordConfirmation,
+  requireEmailExists,
+  requireValidPasswordForUser,
 } = require('./validators');
 
 const router = express.Router();
@@ -52,26 +54,19 @@ router.get('/signin', (req, res) => {
   res.send(signinTemplate());
 });
 
-router.post('/signin', async (req, res) => {
-  const { email, password } = req.body;
-  const user = await usersRepo.getOneBy({ email });
+router.post(
+  '/signin',
+  [requireEmailExists, requireValidPasswordForUser],
+  async (req, res) => {
+    const errors = validationResult(req);
+    console.log(errors);
+    const { email } = req.body;
+    const user = await usersRepo.getOneBy({ email });
 
-  if (!user) {
-    return res.send('Email not found');
+    req.session.userId = user.id;
+
+    res.send('You are signed in');
   }
-
-  const validPassword = await usersRepo.comparePasswords(
-    user.password,
-    password
-  );
-
-  if (!validPassword) {
-    return res.send('Invalid password');
-  }
-
-  req.session.userId = user.id;
-
-  res.send('You are signed in');
-});
+);
 
 module.exports = router;
