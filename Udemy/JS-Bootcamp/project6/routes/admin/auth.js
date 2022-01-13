2a1af29e97be8c3d1ab90a5bae1917a815a3525a
leezer94 <2kunhee94@gmail.com>
2022-01-13 11:24:58 +0900

    chore: add express-validation

--- Udemy/JS-Bootcamp/project6/routes/admin/auth.js
+++ Udemy/JS-Bootcamp/project6/routes/admin/auth.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const { check } = require('express-validator');
+
 const usersRepo = require('../../repositories/users');
 const signupTemplate = require('../views/admin/auth/signup');
 const signinTemplate = require('../views/admin/auth/signin');
@@ -42,27 +44,32 @@ router.get('/signin', (req, res) => {
   res.send(signinTemplate());
 });
 
-router.post('/signin', async (req, res) => {
-  const { email, password } = req.body;
+// Validation
+router.post(
+  '/signin',
+  [check('email'), check('password'), check('passwordConfirmation')],
+  async (req, res) => {
+    const { email, password } = req.body;
 
-  const user = await usersRepo.getOneBy({ email });
+    const user = await usersRepo.getOneBy({ email });
 
-  if (!user) {
-    return res.send('Email not found');
+    if (!user) {
+      return res.send('Email not found');
+    }
+
+    const validPassword = await usersRepo.comparePasswords(
+      user.password,
+      password
+    );
+
+    if (!validPassword) {
+      return res.send('Invalid password');
+    }
+
+    req.session.userId = user.id;
+
+    res.send('You are signed in');
   }
-
-  const validPassword = await usersRepo.comparePasswords(
-    user.password,
-    password
-  );
-
-  if (!validPassword) {
-    return res.send('Invalid password');
-  }
-
-  req.session.userId = user.id;
-
-  res.send('You are signed in');
-});
+);
 
 module.exports = router;
