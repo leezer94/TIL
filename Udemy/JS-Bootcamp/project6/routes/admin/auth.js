1eadd76220a0b46ded144e4460fb181b54ede532
leezer94 <2kunhee94@gmail.com>
2022-01-16 13:47:05 +0900

    refactor: make error handling function reusable

--- Udemy/JS-Bootcamp/project6/routes/admin/auth.js
+++ Udemy/JS-Bootcamp/project6/routes/admin/auth.js
@@ -1,6 +1,6 @@
 const express = require('express');
-const { check, validationResult } = require('express-validator');
 
+const { handleErrors } = require('./middlewares');
 const usersRepo = require('../../repositories/users');
 const signupTemplate = require('../views/admin/auth/signup');
 const signinTemplate = require('../views/admin/auth/signin');
@@ -24,14 +24,9 @@ router.get('/signup', (req, res) => {
 router.post(
   '/signup',
   [requireEmail, requirePassword, requirePasswordConfirmation],
+  handleErrors(signupTemplate),
   async (req, res) => {
-    const errors = validationResult(req);
-
-    if (!errors.isEmpty()) {
-      console.log(errors);
-      return res.send(signupTemplate({ req, errors }));
-    }
-    const { email, password, passwordConfirmation } = req.body;
+    const { email, password } = req.body;
     // Create a user in out user repository to represent this person
 
     const user = await usersRepo.create({ email, password });
@@ -58,13 +53,8 @@ router.get('/signin', (req, res) => {
 router.post(
   '/signin',
   [requireEmailExists, requireValidPasswordForUser],
+  handleErrors(signinTemplate),
   async (req, res) => {
-    const errors = validationResult(req);
-
-    if (!errors.isEmpty()) {
-      return res.send(signinTemplate({ errors }));
-    }
-
     const { email } = req.body;
     const user = await usersRepo.getOneBy({ email });
 
