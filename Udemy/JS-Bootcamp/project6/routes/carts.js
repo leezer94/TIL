4f7e584955160402deac0c883100f1b887b37600
leezer94 <2kunhee94@gmail.com>
2022-01-17 20:13:16 +0900

    feat: create cart and implement product details

--- Udemy/JS-Bootcamp/project6/routes/carts.js
+++ Udemy/JS-Bootcamp/project6/routes/carts.js
@@ -1,5 +1,35 @@
 const express = require('express');
+const cartsRepo = require('../repositories/carts');
 
 const router = express.Router();
+
+// Receive a post request to add an item to a carts
+
+router.post('/cart/products', async (req, res) => {
+  // Figure out the cart!
+  let cart;
+  if (!req.session.cartId) {
+    // We dont have a cart, we need to create one
+    // and store the cart id on the req.session.cartId
+    //property
+
+    cart = await cartsRepo.create({ items: [] });
+
+    req.session.cartId = cart.id;
+  } else {
+    // We have a cart! Lets get it from the repository
+    cart = await cartsRepo.getOne(req.session.cartId);
+  }
+
+  console.log(cart);
+
+  // Either increment quantity for existing product
+  // OR add new product items array
+
+  res.send('Product added to cart');
+});
+// Receive a GET request to show all items in carts
+
+// Receive a post request to delete an item from a cart
 
 module.exports = router;
