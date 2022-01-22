6f24511e1c7b1c993fed5646e33e539a68686c37
leezer94 <2kunhee94@gmail.com>
2022-01-22 16:52:46 +0900

    feat: test if input is exist

--- Udemy/JS-Bootcamp/testing/tme/runner.js
+++ Udemy/JS-Bootcamp/testing/tme/runner.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 const path = require('path');
-
+const render = require('./render');
 const forbiddenDirs = ['node_modules'];
 
 class Runner {
@@ -12,6 +12,7 @@ class Runner {
     for (let file of this.testFiles) {
       console.log(`---- ${file.shortName} ----`);
       const beforeEaches = [];
+      global.render = render;
       global.beforeEach = (fn) => {
         beforeEaches.push(fn);
       };
