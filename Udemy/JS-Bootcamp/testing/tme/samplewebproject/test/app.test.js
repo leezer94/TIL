5671430d14af62d62c41d59c8b5d8076287fb14a
leezer94 <2kunhee94@gmail.com>
2022-01-22 17:00:34 +0900

    feat: add test for h1 when user put right email it is suppoesed to show the innerHTML

--- Udemy/JS-Bootcamp/testing/tme/samplewebproject/test/app.test.js
+++ Udemy/JS-Bootcamp/testing/tme/samplewebproject/test/app.test.js
@@ -1,9 +1,22 @@
 const assert = require('assert');
+const render = require('../../render');
 
 it('has a text input', async () => {
   const dom = await render('index.html');
-
   const input = dom.window.document.querySelector('input');
 
   assert(input);
+});
+
+it('shows a success message with a valid email', async () => {
+  const dom = await render('index.html');
+  const input = dom.window.document.querySelector('input');
+  const h1 = dom.window.document.querySelector('h1');
+
+  input.value = '2kunhee94@gmail.com';
+  dom.window.document
+    .querySelector('form')
+    .dispatchEvent(new dom.window.Event('submit'));
+
+  console.log('content of h1', h1.innerHTML);
 });
