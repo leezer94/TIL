a264f3a8b660691bc7a70467d31fd9aa82fa6189
leezer94 <2kunhee94@gmail.com>
2022-02-01 15:42:26 +0900

    programmers problem

--- programmers/Easy.js
+++ programmers/Easy.js
@@ -544,5 +544,66 @@
   // let s = '2three45sixsevenzero';
   let s = '1zerotwozero3zero';
 
-  console.log(solution(s));
+  // console.log(solution(s));
+}
+{
+  // 신규 아이디 추천
+
+  function solution(new_id) {
+    // 1단계 new_id의 모든 대문자를 대응되는 소문자로 치환합니다.
+    new_id = new_id.toLowerCase();
+    // 2단계 new_id에서 알파벳 소문자, 숫자, 빼기(-), 밑줄(_), 마침표(.)를 제외한 모든 문자를 제거합니다.
+    new_id = new_id
+      .split('')
+      .filter(
+        (cha) =>
+          cha === '-' || cha === '.' || cha === '_' || cha.match(/[a-z]/g)
+      )
+      .join('');
+    // 3단계 new_id에서 마침표(.)가 2번 이상 연속된 부분을 하나의 마침표(.)로 치환합니다.
+    new_id = new_id.replace(/\.+/g, '.');
+    // 4단계 new_id에서 마침표(.)가 처음이나 끝에 위치한다면 제거합니다.
+    if (new_id[0] === '.') {
+      new_id = new_id.slice(1, new_id.length);
+    }
+    if (new_id[new_id.length - 1] === '.') {
+      new_id = new_id.slice(new_id.length);
+    }
+
+    // 5단계 new_id가 빈 문자열이라면, new_id에 "a"를 대입합니다.
+    if (new_id === '') {
+      new_id = 'a';
+    }
+    // 6단계 new_id의 길이가 16자 이상이면, new_id의 첫 15개의 문자를 제외한 나머지 문자들을 모두 제거합니다.
+    // 만약 제거 후 마침표(.)가 new_id의 끝에 위치한다면 끝에 위치한 마침표(.) 문자를 제거합니다.
+    if (new_id.length >= 16) {
+      new_id = new_id.slice(0, 15);
+    }
+    if (new_id[new_id.length - 1] === '.') {
+      new_id = new_id.slice(new_id.length);
+    }
+
+    // 7단계 new_id의 길이가 2자 이하라면, new_id의 마지막 문자를 new_id의 길이가 3이 될 때까지 반복해서 끝에 붙입니다.
+
+    if (new_id.length <= 2) {
+      while (new_id.length === 3) {
+        new_id.push(new_id[new_id.length - 1]);
+      }
+    }
+
+    return new_id;
+
+    //     const answer = new_id
+    //     .toLowerCase() // 1
+    //     .replace(/[^\w-_.]/g, '') // 2
+    //     .replace(/\.+/g, '.') // 3
+    //     .replace(/^\.|\.$/g, '') // 4
+    //     .replace(/^$/, 'a') // 5
+    //     .slice(0, 15).replace(/\.$/, ''); // 6
+    // const len = answer.length;
+    // return len > 2 ? answer : answer + answer.charAt(len - 1).repeat(3 - len);
+  }
+  let new_id = '...!@BaT#*..y.abcdefghijklm';
+
+  console.log(solution(new_id));
 }
