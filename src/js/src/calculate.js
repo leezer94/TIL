f104d2e0c89a70ca9cc2594de703a02849df440a
leezer94 <2kunhee94@gmail.com>
2022-02-02 17:32:31 +0900

    feat: = 버튼 클릭시 입력된 숫자 계산 첫번째 입력 숫자와 두번째 입력숫자 로컬스토리지에서 가져와서 연산된 값 디스플레이에 출력

---
+++ src/js/src/calculate.js
@@ -0,0 +1,20 @@
+import { $total } from './DOM.js';
+
+const addSecondNum = () => {
+  const sheet = JSON.parse(localStorage.getItem('array'));
+
+  sheet.numTwo = $total.innerText;
+
+  localStorage.setItem('array', JSON.stringify(sheet));
+};
+
+export const calculate = () => {
+  addSecondNum();
+  const sheet = JSON.parse(localStorage.getItem('array'));
+  const { numOne, numTwo, operator } = sheet;
+
+  if (operator === '+') $total.innerText = Number(numOne) + Number(numTwo);
+  if (operator === '-') $total.innerText = Number(numOne) - Number(numTwo);
+  if (operator === 'X') $total.innerText = Number(numOne) * Number(numTwo);
+  if (operator === '/') $total.innerText = Number(numOne) / Number(numTwo);
+};
