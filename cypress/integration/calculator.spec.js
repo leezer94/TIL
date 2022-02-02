597d7d60300ac60ee026000ae5a1e531a9843788
leezer94 <2kunhee94@gmail.com>
2022-02-02 23:31:30 +0900

    add test case: 두 숫자 나눗셈  테스트 추가

--- cypress/integration/calculator.spec.js
+++ cypress/integration/calculator.spec.js
@@ -54,4 +54,17 @@ describe('calculator', () => {
     cy.get('.operation').contains('=').click();
     cy.get('#total').should('contain', '1');
   });
+
+  it('should be able to calculate division of two numbers', () => {
+    cy.get('.digits').contains('1').click();
+    cy.get('.digits').contains('0').click();
+    cy.get('.digits').contains('0').click();
+    cy.get('#total').should('contain', '100');
+    cy.get('.operation').contains('/').click();
+    cy.get('#total').should('contain', '0');
+    cy.get('.digits').contains('9').click();
+    cy.get('.digits').contains('9').click();
+    cy.get('.operation').contains('=').click();
+    cy.get('#total').should('contain', '1');
+  });
 });
